afb809474dae1cb50af2e4991573cef81a96a633
zorritogitub <zorritogitub@gmail.com>
2026-02-28 09:06:50 -0500

    Update Strings.cs

--- Launcher/HelpClasses/Strings.cs
+++ Launcher/HelpClasses/Strings.cs
@@ -1,29 +1,29 @@
-﻿namespace Launcher.HelpClasses
+namespace Launcher.HelpClasses
 {
     internal class Strings
     {
         public class Info
         {
-            public static readonly string NOT_SET = "Не задано";
+            public static readonly string NOT_SET = "No especificado";
 
-            public static readonly string PATH_CHOOSE = "Выберите папку с клиентом игры";
+            public static readonly string PATH_CHOOSE = "Seleccione la carpeta del cliente del juego";
 
-            public static readonly string INIT = "Инициализация…";
+            public static readonly string INIT = "Inicializando…";
 
-            public static readonly string UPDATE_IN_PROGRESS = "Идет обновление…";
-            public static readonly string UPDATE_DONE = "Игра обновлена";
+            public static readonly string UPDATE_IN_PROGRESS = "Actualizando…";
+            public static readonly string UPDATE_DONE = "Juego actualizado";
         }
 
         public class Error
         {
-            public static readonly string CONNECTION_ERROR = "Ошибка подключения";
-            public static readonly string CONNECTION_ERROR_EXPLAIN = "Невозможно подключиться к сети интернет, проверьте ваше подключение и повторите попытку";
+            public static readonly string CONNECTION_ERROR = "Error de conexión";
+            public static readonly string CONNECTION_ERROR_EXPLAIN = "No se puede conectar a Internet, verifique su conexión e intente nuevamente";
 
-            public static readonly string PATH_ERROR = "Ошибка расположения";
-            public static readonly string PATH_ERROR_EXPLAIN = "Файл \"Wow.exe\" не найден!\nПожалуйста посместите программу в папку с игрой или укажите путь к папке с игрой!\n\nУказать путь сейчас?";
+            public static readonly string PATH_ERROR = "Error de ubicación";
+            public static readonly string PATH_ERROR_EXPLAIN = "¡Archivo \"Wow.exe\" no encontrado!\nPor favor, coloque el programa en la carpeta del juego o indique la ruta a la carpeta del juego.\n\n¿Indicar la ruta ahora?";
 
-            public static readonly string PATH_CHOOSE_ERROR = "Ошибка выбора папки";
-            public static readonly string PATH_CHOOSE_ERROR_EXPLAIN = "В выбранной папке файл \"Wow.exe\" не найден!\nПожалуйста выберите корректную папку с игрой!\n\nПовторить попытку выбора?";
+            public static readonly string PATH_CHOOSE_ERROR = "Error al seleccionar carpeta";
+            public static readonly string PATH_CHOOSE_ERROR_EXPLAIN = "¡En la carpeta seleccionada no se encontró el archivo \"Wow.exe\"!\nPor favor, seleccione una carpeta válida del juego.\n\n¿Intentar seleccionar nuevamente?";
         }
 
     }
